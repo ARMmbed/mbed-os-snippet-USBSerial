a2fc503e0613a9f161182763e9bc04fefed1c650
adbridge <anna.bridge@arm.com>
2020-05-21 17:07:08 +0100

    Mirrored from master snippet repository

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "mbed.h"
+#include "USBSerial.h"
+
+//Virtual serial port over USB
+USBSerial serial;
+
+int main(void)
+{
+    while (1) {
+        serial.printf("I am a virtual serial port\r\n");
+        ThisThread::sleep_for(1000);
+    }
+}
